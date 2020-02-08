1e17acbcdaa6ba50db069d38c3f6fddf99c40c02
winstoncwang <congwang@live.co.uk>
2020-02-08 14:19:01 +0000

    V 0.14
    responding request with a form block

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('hi there');
+	res.send(`
+	<div>
+		<label>Sign Up</label>
+		<form>
+		<input type="text" placeholder="Email">
+		<input type="password" placeholder="Password">
+		<input type="password" placeholder="Confirmation Password">
+		<button>Sign Up</button>
+		</form>
+	</div>
+	`);
 });
 
 app.listen(3000, () => {
