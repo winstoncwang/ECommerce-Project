6e27cfa6e507b65befa54b98369e9d97af9274ce
winstoncwang <congwang@live.co.uk>
2020-02-09 13:12:52 +0000

    V 0.17
    example of middleware of body parsing technique without edge cases.

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = (req, res, next) => {
+	if (req.method === 'POST') {
+		req.on('data', (data) => {
+			const parsedData = data.toString('utf8').split('&');
+			const formData = {};
+			parsedData.forEach((string) => {
+				const [ key, value ] = string.split('=');
+				formData[key] = value;
+			});
+
+			req.body = formData;
+			next(); //checks for POST method, if true parse the string and continue with next() function
+		});
+	} else {
+		next(); //else continue with next().
+	}
+};
+
 app.get('/', (req, res) => {
 	res.send(`
 	<div>
@@ -17,16 +35,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-	req.on('data', (data) => {
-		const parsedData = data.toString('utf8').split('&');
-		const formData = {};
-		parsedData.forEach((string) => {
-			const [ key, value ] = string.split('=');
-			formData[key] = value;
-		});
-
-		console.log(formData);
-	});
 	res.send('Account Created!!');
 });
 
