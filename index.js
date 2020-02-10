42a79bb62a839b43144b339c7b9b7c4d20a5f9d3
winstoncwang <congwang@live.co.uk>
2020-02-10 08:18:45 +0000

    V 0.18
    express body-parser middleware implemented.

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const bodyParser = (req, res, next) => {
-	if (req.method === 'POST') {
-		req.on('data', (data) => {
-			const parsedData = data.toString('utf8').split('&');
-			const formData = {};
-			parsedData.forEach((string) => {
-				const [ key, value ] = string.split('=');
-				formData[key] = value;
-			});
+// const bodyParser = (req, res, next) => {
+// 	if (req.method === 'POST') {
+// 		req.on('data', (data) => {
+// 			const parsedData = data.toString('utf8').split('&');
+// 			const formData = {};
+// 			parsedData.forEach((string) => {
+// 				const [ key, value ] = string.split('=');
+// 				formData[key] = value;
+// 			});
 
-			req.body = formData;
-			next(); //checks for POST method, if true parse the string and continue with next() function
-		});
-	} else {
-		next(); //else continue with next().
-	}
-};
+// 			req.body = formData;
+// 			next(); //checks for POST method, if true parse the string and continue with next() function
+// 		});
+// 	} else {
+// 		next(); //else continue with next().
+// 	}
+// };
 
 app.get('/', (req, res) => {
 	res.send(`
@@ -34,7 +35,8 @@ app.get('/', (req, res) => {
 	`);
 });
 
-app.post('/', (req, res) => {
+app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
+	console.log(req.body);
 	res.send('Account Created!!');
 });
 
