369b2490088f253e10f441d53264cf0048b0f845
winstoncwang <congwang@live.co.uk>
2020-02-17 15:45:09 +0000

    V 0.24
    add create() and push data into the data storage

--- repositories/users.js
+++ repositories/users.js
@@ -21,16 +21,32 @@ class UsersRepository {
 		//read its content
 		//parse json data
 		//return json data
-		return (fileData = JSON.parse(
+		return JSON.parse(
 			await fs.promises.readFile(this.filename, {
 				encoding : 'utf8'
 			})
-		));
+		);
+	}
+
+	async create (attrs) {
+		//read existing json file
+		const currentRecords = await this.getAll();
+		currentRecords.push(attrs);
+		await fs.promises.writeFile(
+			this.filename,
+			JSON.stringify(currentRecords)
+		);
 	}
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
+	await repo.create({
+		FirstName  : 'bob',
+		SecondName : 'dilly',
+		UserName   : 'bobod',
+		Password   : 'password'
+	});
 	const users = await repo.getAll();
 	console.log(users);
 };
