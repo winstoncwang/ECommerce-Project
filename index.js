998bef5f04ae07ac232781dea7635bc1dc271199
winstoncwang <congwang@live.co.uk>
2020-03-18 18:12:05 +0000

    V 0.444
    public file to wire up css format etc

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
+//express middleware that looks into public folder and make everything available.
+app.use(express.static('public'));
+//parses the buffer info from RESTAPI and parse the data, push into req.body
 app.use(bodyParser.urlencoded({ extended: true }));
+//middleware that deals with cookiesession. which is used to identify user and ensure the right
+//person is signed in/out
 app.use(
 	cookieSession({
 		keys : [ 'asdasds' ]
 	})
 );
+//middle of express.Router, subrouter imported from auth.js
 app.use(authRouter);
 
 app.listen(3000, () => {
