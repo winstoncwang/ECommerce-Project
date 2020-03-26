f5a573d2c32eb875b391b133f1a2ffa151a7705e
winstoncwang <congwang@live.co.uk>
2020-03-26 10:45:29 +0000

    V 0.521
    new product page styling changed

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -33,10 +33,13 @@ router.post(
 	upload.single('image'),
 	[ requireTitle, requirePrice ],
 	async (req, res) => {
-		const err = validationResult(req);
-
-		if (!err.isEmpty()) {
-			res.send(productsNewTemp({ err }));
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.send(productsNewTemp(errors));
+			/*return is necessary here since res.send sends a header to the client notify them 
+			there is a error. if you remove the return statement, everything will continue until res.send(sub). This causes error when the
+			window for accepting res headers are closed. But server continues with more headers. Hence we have, cannot set headers after
+			they are sent to the client (error)*/
 		}
 		const image = req.file.buffer.toString('base64'); //enconding the buffer info. its safer than raw info. but not product ready.
 		const { title, price } = req.body;
