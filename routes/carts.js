8191da9858bb1f98126db2990cd8135c0740ba2d
winstoncwang <congwang@live.co.uk>
2020-04-09 09:25:45 +0100

    V 0.631
    update user facing index page and cart id config

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
+const cartsRepo = require('../repositories/carts');
+
 const router = express.Router();
 
-router.get('/cart/products', async (req, res) => {
-	res.send('carts');
+//route for add to cart button POST
+router.post('/cart/products', async (req, res) => {
+	//check the cart id
+	if (!req.session.cartId) {
+		//if a new user create a cart id
+		const cart = await cartsRepo.create({ items: [] });
+		req.session.cartId = cart.id;
+	} else {
+		//find existing cart id
+		const cart = await cartsRepo.getOne(req.session.cartId);
+	}
 });
+
+//route for display cart products GET
+//route for delete button in cart product page POST
 
 module.exports = router;
