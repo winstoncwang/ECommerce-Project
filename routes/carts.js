089f463a2c1f6bc69c3d7ab300a90580fc3d0922
winstoncwang <51161138+winstoncwang@users.noreply.github.com>
2020-04-14 09:33:14 +0100

    V 0.66
    adding carts deletion route

--- routes/carts.js
+++ routes/carts.js
@@ -1,64 +1,73 @@
-const express = require('express');
+const express = require("express");
 
-const cartsRepo = require('../repositories/carts');
-const productsRepo = require('../repositories/products');
+const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
 
-const cartsShowTemp = require('../views/carts/show');
+const cartsShowTemp = require("../views/carts/show");
 
 const router = express.Router();
 
 //route for add to cart button POST
-router.post('/cart/products', async (req, res) => {
-	//check the cart id
-	let cart;
-	if (!req.session.cartId) {
-		//if a new user create a cart id
-		cart = await cartsRepo.create({ items: [] });
-		req.session.cartId = cart.id;
-	} else {
-		//find existing cart id
-		cart = await cartsRepo.getOne(req.session.cartId);
-	}
+router.post("/cart/products", async (req, res) => {
+  //check the cart id
+  let cart;
+  if (!req.session.cartId) {
+    //if a new user create a cart id
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    //find existing cart id
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
 
-	//find if there is a existingItem
-	const existingItem = cart.items.find(
-		(item) => item.id === req.body.productId
-	);
+  //find if there is a existingItem
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
 
-	if (existingItem) {
-		//increment quantity
-		existingItem.quantity++;
-	} else {
-		//add to items
-		cart.items.push({ id: req.body.productId, quantity: 1 });
-	}
+  if (existingItem) {
+    //increment quantity
+    existingItem.quantity++;
+  } else {
+    //add to items
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
 
-	//update(id, items:cart.items)
-	await cartsRepo.update(cart.id, { items: cart.items });
+  //update(id, items:cart.items)
+  await cartsRepo.update(cart.id, { items: cart.items });
 
-	res.send('product added!');
+  res.send("product added!");
 });
 
 //route for display cart products GET
-router.get('/cart', async (req, res) => {
-	//check if they have a cartId or is an existing user
-	if (!req.session.cartId) {
-		return res.redirect('/');
-	}
+router.get("/cart", async (req, res) => {
+  //check if they have a cartId or is an existing user
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
 
-	const cart = await cartsRepo.getOne(req.session.cartId);
+  const cart = await cartsRepo.getOne(req.session.cartId);
 
-	for (let item of cart.items) {
-		const product = await productsRepo.getOne(item.id);
-		//hand product info to cart object
-		//this is not written into the repo, so no duplicated data.
-		item.product = product;
-	}
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+    //hand product info to cart object
+    //this is not written into the repo, so no duplicated data.
+    item.product = product;
+  }
 
-	console.log(cart.items);
-	res.send(cartsShowTemp({ items: cart.items }));
+  res.send(cartsShowTemp({ items: cart.items }));
 });
 
 //route for delete button in cart product page POST
+router.post("/cart/:id/delete", async (req, res) => {
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  //filter out the req.params.id for the product and write the rest in cart repo
+  const filteredCart = cart.items.filter((item) => {
+    return item.id !== req.params.id;
+  });
+  console.log(filteredCart);
+  await cartsRepo.update(req.session.cartId, { items: filteredCart });
+});
 
 module.exports = router;
--- views/carts/show.js
+++ views/carts/show.js
@@ -1,13 +1,13 @@
-const layout = require('../layout');
+const layout = require("../layout");
 
 module.exports = ({ items }) => {
-	const totalPrice = items.reduce((acc, item) => {
-		return acc + item.quantity * item.product.price;
-	}, 0);
+  const totalPrice = items.reduce((acc, item) => {
+    return acc + item.quantity * item.product.price;
+  }, 0);
 
-	const renderedItems = items
-		.map((item) => {
-			return `
+  const renderedItems = items
+    .map((item) => {
+      return `
           <div class="cart-item message">
             <h3 class="subtitle">${item.product.title}</h3>
             <div class="cart-right">
@@ -18,7 +18,7 @@ module.exports = ({ items }) => {
                 £${item.product.price * item.quantity}
               </div>
               <div class="remove">
-                <form method="POST">
+                <form method="POST" action="/cart/${item.id}/delete">
                   <button class="button is-danger">                  
                     <span class="icon is-small">
                       <i class="fas fa-times"></i>
@@ -29,11 +29,11 @@ module.exports = ({ items }) => {
             </div>
           </div>
         `;
-		})
-		.join('');
+    })
+    .join("");
 
-	return layout({
-		content : `
+  return layout({
+    content: `
         <div id="cart" class="container">
           <div class="columns">
             <div class="column"></div>
@@ -53,6 +53,6 @@ module.exports = ({ items }) => {
             <div class="column"></div>
           </div>
         </div>
-      `
-	});
+      `,
+  });
 };
